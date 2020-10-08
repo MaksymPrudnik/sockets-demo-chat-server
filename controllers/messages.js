38284603359f0caf36a39fe585762228460fe75d
maksymprudnik <maksymprudnik@gmail.com>
2020-10-08 14:03:58 +0300

    added docker and postgres

---
+++ controllers/messages.js
@@ -0,0 +1,33 @@
+const getMessages = async (count, channel, socket, db) => {
+    if (!channel) return socket.emit('load messages fail', 'Wrong request');
+    const isChannelExist = await db('channels')
+        .where('name', channel)
+        .then(channels => Boolean(channels.length))
+    if (!isChannelExist) return socket.emit('load messages fail', 'No such channel')
+    const messages = await db('messages').select().where({channel});
+    socket.emit('messages loaded', messages.slice(
+        Math.max(messages.length - 10 - count, 0), 
+        messages.length - count
+    ))
+}
+
+const addMessages = async (message, socket, io, db) => {
+    if (!message.channel || !message.body.username || !message.body.message ) {
+        return socket.emit('message fail', 'Invalid message')
+    };
+    const messages = await db('messages')
+        .returning(['channel', 'username', 'body', 'createdat'])
+        .insert({
+            channel: message.channel, 
+            username: message.body.username,
+            body: message.body.message,
+            createdat: new Date().toLocaleString()
+        })
+    const newMessage = messages[messages.length-1];
+    return io.emit('message added', newMessage);
+}
+
+module.exports = {
+    addMessages,
+    getMessages
+}
